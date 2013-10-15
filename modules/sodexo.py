349e466238e436560524c1b8a29b8c1825b72689
siikamiika <siikamiika@craptop.local>
2013-10-15 23:26:39 +0300

    ict tukee matiketo ja "koko"

--- modules/sodexo.py
+++ modules/sodexo.py
@@ -1,15 +1,40 @@
 import urllib.request,datetime,json,sys
 
 def lounas(arguments):
+
 	days = ['ma', 'ti', 'ke', 'to', 'pe', 'la', 'su']
 	date = datetime.datetime.now()
+	koko = False
+
 	try:
 		delta = int(arguments[0])
 		date += datetime.timedelta(days=delta)
+		paiva = date.weekday()
 	except:
-		pass
-	ruokalista = json.loads(urllib.request.urlopen('http://www.sodexo.fi/ruokalistat/output/daily_json/427/'+str(date.year)+'/'+str(date.month)+'/'+str(date.day)+'/fi').read().decode())
-	ret = days[date.weekday()] + ': '
-	for ruoka in ruokalista['courses']:
-		ret += '| ' + ruoka['price'].split('/')[0] + ruoka['title_fi'] + ' |'
-	return ret
+		if len(arguments) != 0:
+			if arguments[0] in days:
+				paiva = days.index(arguments[0])
+				date += datetime.timedelta(days=paiva-date.weekday())
+			if arguments[0] == 'koko':
+				koko = True
+				paiva = date.weekday()
+		else:
+			paiva = date.weekday()
+
+	try:
+		ret = ''
+		while 1:
+			ruokalista = json.loads(urllib.request.urlopen('http://www.sodexo.fi/ruokalistat/output/daily_json/427/'+str(date.year)+'/'+str(date.month)+'/'+str(date.day)+'/fi').read().decode())
+			ret += days[date.weekday()] + ': '
+			for ruoka in ruokalista['courses']:
+				ret += '| ' + ruoka['price'].split('/')[0] + ruoka['title_fi'] + ' |'
+			ret += '\n'
+			if koko == False:
+				break
+			if paiva > 4:
+				break
+			date += datetime.timedelta(days=1)
+			paiva += 1
+		return ret
+	except:
+		return 'jotain hajos'
