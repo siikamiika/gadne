b1f664c11d912f4f8239334610d4f8eb707bb59f
siikamiika <siikamiika@craptop.local>
2013-10-20 20:51:02 +0300

    viilausta

--- modules/youtube.py
+++ modules/youtube.py
@@ -5,11 +5,11 @@ import re
 
 def info(url):
 	try:
-		match = re.search('(youtube\.com/watch\?v=|youtube\.com/v/|youtu\.be/)(.*?)(&|$)', url)
+		match = re.search('(youtube\.com/watch\?v=|youtube\.com/v/|youtu\.be/)(.*?)(\?|&|$)', url)
 		videoid = match.group(2)
 		jsondata = json.loads(urllib.request.urlopen('http://gdata.youtube.com/feeds/api/videos/'+videoid+'?v=2&alt=jsonc').read().decode())
 		video = jsondata['data']
-		kesto = ' ('+str(datetime.timedelta(seconds=video['duration']))+')'
+		kesto = ' ('+str(datetime.timedelta(seconds=video['duration'])).lstrip('0:')+')'
 		nippelitieto = ' / '.join(['Aihe: '+video['category'], '{:,}'.format(video['viewCount'])+' katselukertaa', 'likeratio: '+str(video['rating'])])
 		ret = 'Youtube: '
 		ret += '['+video['uploader']+'] '+video['title']+kesto+' | '+nippelitieto
