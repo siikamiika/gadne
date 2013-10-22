79c30e1fd1784fb7a77cab5d12a705cb07b8256d
siikamiika <siikamiika@craptop.local>
2013-10-22 03:44:59 +0300

    ehkä parempi näin

--- modules/youtube.py
+++ modules/youtube.py
@@ -9,7 +9,7 @@ def info(url):
 		videoid = match.group(1)
 		jsondata = json.loads(urllib.request.urlopen('http://gdata.youtube.com/feeds/api/videos/'+videoid+'?v=2&alt=jsonc').read().decode())
 		video = jsondata['data']
-		kesto = ' ('+str(datetime.timedelta(seconds=video['duration'])).lstrip('0:')+')'
+		kesto = ' ('+str(datetime.timedelta(seconds=video['duration']))+')'
 		nippelitieto = ' / '.join(['Aihe: '+video['category'], '{:,}'.format(video['viewCount'])+' katselukertaa', 'likeratio: '+str(video['rating']*20)+'%'])
 		ret = 'Youtube: '
 		ret += '['+video['uploader']+'] '+video['title']+kesto+' | '+nippelitieto
