98c8a1f269b0639690795ccfdbc4964342803814
siikamiika <siikamiika@craptop.local>
2013-10-24 09:43:18 +0300

    ei kysellä sitä ens vuoden säätä

--- modules/turkuweather.py
+++ modules/turkuweather.py
@@ -12,9 +12,11 @@ def weather(arguments):
 		date += datetime.timedelta(days=delta)
 	except:
 		pass
-	turku = json.loads(urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast/daily?q=Turku,fi&mode=json&units=metric').read().decode())
+	turku = json.loads(urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast/daily?q=Turku,fi&mode=json&units=metric&cnt=14').read().decode())
+	ret = 'dunnolol'
 	for weather in turku['list']:
-		if datetime.datetime.fromtimestamp(weather['dt']).day == date.day:
+		weatherdate = datetime.datetime.fromtimestamp(weather['dt'])
+		if weatherdate.day == date.day and weatherdate.month == date.month and weatherdate.year == date.year:
 			temp = weather['temp']
 			ret = date.strftime("%A %d. %B %Y") + ': ' + weather['weather'][0]['description'] + ' | aamu: '+str(temp['morn'])+deg + ' päivä: '+str(temp['day'])+deg + ' ilta: '+str(temp['eve'])+deg + ' yö: '+str(temp['night'])+deg
 
