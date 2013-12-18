42f1dc31844b8ef242fd6421f1acc525686b904b
siikamiika <siikamiika@craptop.local>
2013-12-18 14:45:00 +0200

    ei me mitää python 2 tueta

--- modules/katse.py
+++ modules/katse.py
@@ -6,20 +6,19 @@
 # mää räpeltämäs :gcomp:
 # by fisle
 #
-import urllib2
-import simplejson as json
+import urllib.request
+import json
 
 def katse(juuh):
-        # Katse se BTC-e API
-        if katse == 'ltc':
-                URL = 'https://btc-e.com/api/2/ltc_usd/ticker'
-        elif katse == 'btc':
-                URL = 'https://btc-e.com/api/2/btc_usd/ticker'
-        response = urllib2.urlopen(URL)
-        html = response.read()
-        # Katse se JSON KUULIKKO
-        data = json.loads(html)
-        last = data['ticker']['last']
-        last = 'Iham hyvä 1 {!s} = ${!s}, mitä mieltä muut :rolleyes:'.format(juuh, last)
-        # Kuulim :o
-        return last
+    # Katse se BTC-e API
+    if juuh == 'ltc':
+        URL = 'https://btc-e.com/api/2/ltc_usd/ticker'
+    elif juuh == 'btc':
+        URL = 'https://btc-e.com/api/2/btc_usd/ticker'
+    gconf = urllib.request.urlopen(URL).read().decode()
+    # Katse se JSON KUULIKKO
+    data = json.loads(gconf)
+    last = data['ticker']['last']
+    last = 'Iham hyvä 1 {!s} = ${!s}, mitä mieltä muut :rolleyes:'.format(juuh, last)
+    # Kuulim :o
+    return last
