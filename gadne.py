e9a04fcbfc98f9a6bcca0663fae21100e7ac77cd
unarr <unarr@fisle.eu>
2013-12-23 13:07:32 +0200

    Ainii se komento myös

--- gadne.py
+++ gadne.py
@@ -52,6 +52,8 @@ class MUCBot(sleekxmpp.ClientXMPP):
                 viesti = modules.katse.katse('btc')
             if msg_args[0] == '!ltc':
                 viesti = modules.katse.katse('ltc')
+            if msg_args[0] == '!xpm':
+                viesti = modules.katse.katse('xpm')
             if msg_args[0] == '!assari':
                 viesti = modules.unica.lounas('assarin-ullakko/', msg_args[1:])
             if msg_args[0] == '!ict':
