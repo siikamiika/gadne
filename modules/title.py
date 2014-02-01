7f0085634e711d6a48ef35bc830950e95cd45539
siikamiika <siikamiika@craptop.local>
2014-02-01 16:17:25 +0200

    regression

--- modules/title.py
+++ modules/title.py
@@ -3,13 +3,16 @@ import urllib.request
 import html.parser
 
 def get(url):
-    request = urllib.request.Request(url)
-    request.add_header(
-        'User-Agent',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) '
-        'Chrome/19.0.1084.9 Safari/536.5')
-    response = urllib.request.urlopen(request)
-    page = response.read(1024*1024)
+    try:
+        request = urllib.request.Request(url)
+        request.add_header(
+            'User-Agent',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) '
+            'Chrome/19.0.1084.9 Safari/536.5')
+        response = urllib.request.urlopen(request)
+        page = response.read(1024*1024)
+    except Exception as e:
+        return ''
     encoding = response.headers.get_content_charset()
     if encoding is None:
         charset = (
