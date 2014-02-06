e08006231f99cbcd0c4976009181da2078a0e4ba
siikamiika <siikamiika@craptop.local>
2014-02-06 13:47:33 +0200

    special cases

--- modules/each_word/link.py
+++ modules/each_word/link.py
@@ -5,6 +5,7 @@ import html.parser
 import json
 import zlib
 from datetime import timedelta
+import time
 import html5lib
 from bs4 import BeautifulSoup
 
@@ -25,7 +26,7 @@ def run(url):
         # collect info from URL
         request = urllib.request.Request(url)
         request.add_header('User-Agent', user_agent)
-        response = urllib.request.urlopen(request)
+        response = urllib.request.urlopen(request, timeout=10)
         content_type = response.headers['Content-Type'].lower()
         encoding = response.headers.get_content_charset()
         redir = response.geturl()
@@ -43,7 +44,7 @@ def run(url):
                 'searchbyimage?&image_url='+urllib.parse.quote(url))
             req = urllib.request.Request(googleimg)
             req.add_header('User-Agent', user_agent)
-            result_page = urllib.request.urlopen(req).read()
+            result_page = urllib.request.urlopen(req, timeout=10).read()
             imagedesc = re.search(b'Best guess for this image.*?>(.*?)</a>',
                 result_page, re.M).group(1).decode()
             print(url, 'img:', imagedesc)
@@ -51,11 +52,17 @@ def run(url):
         except AttributeError:
             print(url, 'no image description')
             return
-
+        except Exception as e:
+            print(url, e)
+            return
 
     if rtmatch:
         with open('modules/each_word/gadnex.login', 'r') as login:
-            usr, pw = login.read().split(':')
+            try:
+                usr, pw = login.read().split(':')
+            except Exception as e:
+                print('Error reading username and password:', e)
+                return
         if '#' in url:
             post_id = url[1+url.index('#'):]
         else:
@@ -73,21 +80,29 @@ def run(url):
                 'vb_login_username': usr,
                 'vb_login_password': pw,
                 'do': 'login'
-            }).encode()
+            }).encode('windows-1252', 'ignore')
 
         def get_page(cookies):
             rt_headers = dict(login_headers)
             rt_headers.pop('Origin')
             rt_headers['Cookie'] = cookies
-            rt_req = urllib.request.Request(redir, headers=rt_headers)
-            rt_resp = urllib.request.urlopen(rt_req)
-            rt_page = zlib.decompress(rt_resp.read(), 16+zlib.MAX_WBITS)
-            return rt_page
+            try:
+                rt_req = urllib.request.Request(redir, headers=rt_headers)
+                rt_resp = urllib.request.urlopen(rt_req)
+                rt_page = zlib.decompress(rt_resp.read(), 16+zlib.MAX_WBITS)
+                return rt_page
+            except Exception as e:
+                print(e)
+                return
 
         def login():
-            login_req = urllib.request.Request(login_url, login_data, login_headers)
-            login_resp = urllib.request.urlopen(login_req)
-            login_page = zlib.decompress(login_resp.read(), 16+zlib.MAX_WBITS)
+            try:
+                login_req = urllib.request.Request(login_url, login_data, login_headers)
+                login_resp = urllib.request.urlopen(login_req)
+                login_page = zlib.decompress(login_resp.read(), 16+zlib.MAX_WBITS)
+            except Exception as e:
+                print(e)
+                return
             if b'Olet kirjautunut' in login_page:
                 print('MuroBBS login success!')
             else:
@@ -104,28 +119,35 @@ def run(url):
             with open('modules/each_word/cookies.login', 'r') as c:
                 cookies = c.read()
             rt = get_page(cookies)
-            if b'Et ole kirjautunut' in rt:
+            if not rt:
+                return 'Ei voi ladata sivua'
+            elif b'Et ole kirjautunut' in rt:
                 rt = get_page(login())
             if b'Et ole kirjautunut' in rt:
                 return 'Ei voi kirjautua sisään'
         except Exception as e:
             if type(e) == OSError or IOError:
                 rt = get_page(login())
-                if b'Et ole kirjautunut' in rt:
+                if not rt:
+                    return 'Ei voi ladata sivua'
+                elif b'Et ole kirjautunut' in rt:
                     return 'Ei voi kirjautua sisään'
             else:
                 print(e)
                 return
         # debug
-        with open('rt.html', 'wb') as f:
-            f.write(rt)
+        #with open('rt.html', 'wb') as f:
+        #    f.write(rt)
         try:
             rt = BeautifulSoup(rt.decode('windows-1252'), 'html5lib')
         except Exception as e:
             print(e)
             rt = BeautifulSoup(rt.decode('windows-1252', 'ignore'), 'html5lib')
-        title = rt.title.text.strip()[:-len(' - MuroBBS')]
+        title = re.sub('\s+', ' ', rt.title.text.strip()[:-len(' - MuroBBS')])
         posts = rt.find_all('table', {'id': re.compile('post[0-9]*')})
+        if not posts:
+            return str(title)
+
         def post_details(post_n):
             sm_path = 'modules/each_word/smilies.json'
             try:
@@ -155,6 +177,7 @@ def run(url):
             for item in posts[post_n].find(
                     'div', {'id': 'post_message_'+posts[post_n].get('id')[4:]}
                 ).children:
+
                 if hasattr(item, 'get') and item.get('href'):
                     post_msg.append('[{0}]({1})'.format(item.text,
                         urllib.parse.unquote(item.get('href')).replace(
@@ -183,6 +206,7 @@ def run(url):
 
                 'msg': re.sub('\s+', ' ', ' '.join(post_msg).strip())
             }
+
         try:
             p_ids = [p.get('id') for p in posts]
             p = post_details(p_ids.index(post_id) if post_id in p_ids else 0)
@@ -223,7 +247,11 @@ def run(url):
 
     else:
         # don't block by downloading page before detecting youtube link or image
-        page = response.read(1024*1024)
+        try:
+            page = response.read(1024*1024)
+        except Exception as e:
+            print(url, e)
+            return
 
         if encoding is None:
             # search encoding from meta tag
@@ -233,7 +261,11 @@ def run(url):
                 # utf-8 fallback
                 encoding = 'utf-8'
             else:
-                encoding = charset.group(1).decode()
+                try:
+                    encoding = charset.group(1).decode()
+                except Exception as e:
+                    print(e)
+                    return
 
         title = re.search(b'<title.*?>(.*?)</title>', page, re.S | re.I)
         print('url: {0} redir: {1} encoding: {2} Title: {3}'.format(
