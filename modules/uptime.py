8877c6d099c9cec19d46352b4a74a9a439c5d38a
siikamiika <siikamiika@espowered.local>
2014-02-14 23:38:49 +0200

    xmpp server uptime

--- modules/uptime.py
+++ modules/uptime.py
@@ -1,10 +1,19 @@
 import datetime
+from urllib.request import urlopen
 
 triggers = ['!uptime']
 
 def run(msg):
+    uptime_ = False
+    target = msg['body'].split()
+    if len(target[1:]):
+        if target[1] == 'eddykk':
+            uptime_ = urlopen('https://eddykaykay.pw/uptime_gadne.php').read()
     try:
-        uptime = open('/proc/uptime', 'r').read()
+        if uptime_:
+            uptime = uptime_
+        else:
+            uptime = open('/proc/uptime', 'r').read()
         uptime = float(uptime.split()[0])
         uptime = round(uptime, 0)
         uptime = str(datetime.timedelta(seconds=uptime))
