bb5ae95d12110b9a96982460456996169b05c23a
siikamiika <siikamiika@users.noreply.github.com>
2014-03-11 14:46:48 +0200

    bussi: haku paikan nimellä

--- modules/bus.py
+++ modules/bus.py
@@ -1,7 +1,11 @@
-import urllib.request
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 triggers = ['!bus']
+
+def get_soup(url):
+    bussi = urlopen(url).read().decode('latin-1')
+    return BeautifulSoup(bussi)
 
 def run(msg):
     arguments = msg['body'].split()[1:]
@@ -22,18 +26,28 @@ def run(msg):
     else:
         stop = arguments[0]
 
-    bussi = urllib.request.urlopen(
+    retstring = ''
+
+    try:
+        soppa = get_soup(
             'http://turku.seasam.com/nettinaytto/web'
             '?stopid={}&command=quicksearch&view=mobile'.format(stop)
-        ).read().decode('latin-1')
-    soppa = BeautifulSoup(bussi)
-    ajat = soppa.find('tbody').find_all('tr')[1:]
-    retstring = ''
-    for aika in ajat:
-        retstring += '\n'+aika.find(
-            'td', {'class':'timecol'}).next_element.ljust(6)+' | '
-        retstring += aika.find(
-            'td', {'class':'linecol'}).next_element.ljust(3)+' | '
-        retstring += aika.find(
-            'td', {'class':'destcol'}).next_element.ljust(20)+'    '
+            )
+        ajat = soppa.find('tbody').find_all('tr')[1:]
+        for aika in ajat:
+            retstring += '\n'+aika.find(
+                'td', {'class':'timecol'}).next_element.ljust(6)+' | '
+            retstring += aika.find(
+                'td', {'class':'linecol'}).next_element.ljust(3)+' | '
+            retstring += aika.find(
+                'td', {'class':'destcol'}).next_element.ljust(20)
+    except Exception as e:
+        if type(e) == AttributeError:
+            soppa = get_soup(
+                'http://turku.seasam.com/nettinaytto/web?pysnimitaisos={}'
+                '&linjatunnus=&command=search&view=mobile'.format(stop)
+                )
+            stoplist = soppa.find('div', {'id': 'result-list'})
+            stoplist = [a.text for a in stoplist.find_all('a')]
+            retstring = ', '.join(stoplist)
     return retstring
