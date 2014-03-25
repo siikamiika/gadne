c3b2bac869b09838455dbf5c374cd34b65c284ed
siikamiika <siikamiika@users.noreply.github.com>
2014-03-26 00:15:23 +0200

    vähän voi spoilata

--- modules/each_msg/nonyt.py
+++ modules/each_msg/nonyt.py
@@ -15,15 +15,15 @@ def run(msg):
             return ':laihduta:'
         if a.lower().startswith('feel') or a.lower().startswith('tajuu'):
             return 'Yea, feel me. The beat is all in me.'
-        # ei spoilata
-        if a in \
-            {'clegane', 'bran', 'joffrey', 'catelyn', 'tyrell', 'brienne',
-            'jeor', 'sansa', 'tywin', 'tarly', 'jon', 'wright', 'sandor',
-            'waldau', 'arya', 'samwell', 'daenerys', 'melisandre', 'olenna',
-            'shae', 'gendry', 'jorah', 'margaery', 'seaworth', 'bolton',
-            'mormont', 'tyrion', 'petyr', 'tormund', 'stannis', 'viserys',
-            'ygritte', 'tarth', 'robb', 'cersei', 'lannister', 'eddard',
-            'ramsay', 'snow', 'greyjoy', 'davos', 'theon', 'baratheon',
-            'roose', 'targaryen', 'bronn', 'drogo', 'stark', 'varys',
-            'baelish', 'jaime', 'khal', 'robert', 'talisa', 'thrones'}:
-            return 30*'EI SPOILATA :kasetti:\n' + msg['mucnick'] + ' perkele'
+        # # ei spoilata
+        # if a.lower() in \
+        #     {'clegane', 'bran', 'joffrey', 'catelyn', 'tyrell', 'brienne',
+        #     'jeor', 'sansa', 'tywin', 'tarly', 'jon', 'wright', 'sandor',
+        #     'waldau', 'arya', 'samwell', 'daenerys', 'melisandre', 'olenna',
+        #     'shae', 'gendry', 'jorah', 'margaery', 'seaworth', 'bolton',
+        #     'mormont', 'tyrion', 'petyr', 'tormund', 'stannis', 'viserys',
+        #     'ygritte', 'tarth', 'robb', 'cersei', 'lannister', 'eddard',
+        #     'ramsay', 'snow', 'greyjoy', 'davos', 'theon', 'baratheon',
+        #     'roose', 'targaryen', 'bronn', 'drogo', 'stark', 'varys',
+        #     'baelish', 'jaime', 'khal', 'robert', 'talisa', 'thrones'}:
+        #     return 30*'EI SPOILATA :kasetti:\n' + msg['mucnick'] + ' perkele'
