c10839096be968cc92b1da69137980bc52980f15
siikamiika <siikamiika@users.noreply.github.com>
2015-09-14 15:06:50 +0300

    add opnum

---
+++ modules/opnum.py
@@ -0,0 +1,22 @@
+import os
+
+triggers = ['!opnum']
+
+def run(msg):
+    msg_args = msg['body'].split()
+    storage_path = '{}.opnum'.format(msg['mucnick'])
+
+    if len(msg_args) > 1:
+        if msg_args[1] == 'set':
+            with open(storage_path, 'w') as f:
+                f.write(msg_args[2])
+        else:
+            nick = msg_args[1]
+            if nick + '.opnum' in os.listdir():
+                with open(nick+'.opnum') as f:
+                    return f.read()
+            else:
+                return '(not set)'
+    else:
+        return 'usage: !opnum [<nick>|set 12345]'
+
