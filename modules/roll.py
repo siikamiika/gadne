7f23fdbac77b4873d77eabe3499da4fa2280ea67
siikamiika <siikamiika@users.noreply.github.com>
2015-09-17 13:39:08 +0300

    fix roll

--- modules/roll.py
+++ modules/roll.py
@@ -5,17 +5,22 @@ import re
 triggers = ['!roll']
 
 def _int(i):
-    try: return int(i)
+    try: return int(i.replace(' ', ''))
     except: return None
 
 def run(msg):
-    msg_args = msg['body'].split()
-    times, sides, add = re.search('(\d+)?d(\d+)([+-]\d+)?', msg_args[1]).groups()
+    msg_args = msg['body'].split(maxsplit=1)
+    times, sides, add = re.search('(\d+)?d(\d+)\s*([+-]\s*\d+)?', msg_args[1]).groups()
     times = _int(times) or 1
     sides = int(sides)
     add = _int(add) or 0
-    output = []
+    rolls = []
     for t in range(times):
-        output.append(str(random.randint(1, sides) + add))
-    return ', '.join(output)
+        roll = random.randint(1, sides)
+        rolls.append(roll)
+    total = sum(rolls + [add])
+    output = list(map(str, rolls))
+    if add != 0:
+        output.append('({})'.format(add))
+    return '{} = {}'.format(' + '.join(output), total)
 
