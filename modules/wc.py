2929c91d5934b176a3b0781c95873250caf0089c
siikamiika <siikamiika@users.noreply.github.com>
2015-12-12 23:01:01 +0200

    wc.py: rewrite
    new: regex with !ewc

--- modules/wc.py
+++ modules/wc.py
@@ -1,20 +1,45 @@
 import re
-from collections import Counter
 
-triggers = ['!wc']
+triggers = ['!wc', '!ewc']
 
+def run(cmd):
 
-def run(msg):
-    viesti = msg['body'][4:]
+    pattern = cmd['body'].split(maxsplit=1)
+    if len(pattern) > 1:
+        pattern = pattern[1]
+    else:
+        pattern = ''
+
     try:
         with open('chatlog.log', 'rb') as chatlog:
             chatlog = chatlog.read().decode()
     except IOError:
         return 'ei voi lukea chatlogia'
-    rivit = [rivi for rivi in chatlog.split('\n') if '!wc' not in rivi]
-    nimet = re.findall(
-        '(?<=\/)(\w{3,})(?=\/)(?:.*)(?:' +
-        re.escape(viesti)+')', '\n'.join(rivit)
-        )
-    wc = Counter(nimet)
-    return ', '.join([tup[0]+': '+str(tup[1]) for tup in wc.most_common()])
+
+    counter = dict()
+
+    if cmd['body'].split()[0] == '!wc':
+        if pattern == '':
+            pattern = '^.*?$'
+        else:
+            pattern = re.escape(pattern)
+
+    pattern = re.compile(pattern)
+
+    for row in chatlog.splitlines():
+        if re.search('!e?wc', row):
+            continue
+
+        row = row.split(maxsplit=3)
+        nick = row[2].split('/')[1]
+        msg = row[3]
+
+        matches = pattern.findall(msg)
+
+        if matches:
+            if counter.get(nick):
+                counter[nick] += len(matches)
+            else:
+                counter[nick] = len(matches)
+
+    return ', '.join('{}: {}'.format(*p) for p in sorted(counter.items(), key=lambda x: x[1], reverse=True))
